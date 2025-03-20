ac4dbb505cb06f1c5af001d6cc7b78ec10940a52
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-03-20 16:17:23 +0100

    init: Create project and configured native wind, react, expo

--- App.tsx
+++ App.tsx
@@ -2,12 +2,26 @@ import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
+import {Text, View} from "react-native";
+import {Link} from "expo-router";
 
 export default function App() {
-  return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+
+            <Text className="font-bold text-lg my-10 ">Welcome to ReState</Text>
+
+            <Link href="/app/sign-in">Sign In</Link>
+            <Link href="/app/(root)/(tabs)/explore">Explore</Link>
+            <Link href="/app/(root)/(tabs)/profile">Profile</Link>
+            <Link href="/app/(root)/properties/[id]">Property</Link>
+
+        </View>
+    );
 }
